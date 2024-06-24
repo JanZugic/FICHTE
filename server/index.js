8efa20b28f9e65a63666b595e03cd130ed5e0c42
ZUGA <38326450+JanZugic@users.noreply.github.com>
2024-06-24 19:55:20 +0200

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,48 +1,53 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
+const express = require('express');
+const app = express();
+const PORT = 5000;
 
-const http = require('http').Server(app)
-const cors = require('cors')
+const http = require('http').Server(app);
+const cors = require('cors');
 const socketIO = require('socket.io')(http, {
-    cors:{
+    cors: {
         origin: 'http://localhost:5173'
     }
-})
+});
 
-app.get('api', (req, res) => {
+// Include db.js
+const db = require('./db');
+
+app.get('/api', (req, res) => {
     res.json({
         message: 'Hello'
-    })
-})
+    });
+});
 
-const users = []
+const users = [];
 
 socketIO.on('connection', (socket) => {
-    console.log(socket.id + ' user connected')
-    socket.on('message', (data) => {
-        socketIO.emit('response', data)
-    })
-    socket.on('newUser', (data) => {
-        users.push(data)
-        socketIO.emit('responseNewUser', users)
-    })
+    console.log(socket.id + ' user connected');
 
-    socket.on('typing', (data) => socket.broadcast.emit('responseTyping', data))
+    socket.on('message', (data) => {
+        socketIO.emit('response', data);
+    });
+
+    socket.on('newUser', (data) => {
+        users.push(data);
+        socketIO.emit('responseNewUser', users);
+    });
+
+    socket.on('typing', (data) => socket.broadcast.emit('responseTyping', data));
 
     socket.on('createRoom', (roomName) => {
         // Создаем объект комнаты
         const newRoom = {
-          name: roomName,
-          members: [],
-          creator: socket.id, // Пока используем socket.id как идентификатор создателя
-          messages: [],
+            name: roomName,
+            members: [],
+            creator: socket.id, // Пока используем socket.id как идентификатор создателя
+            messages: [],
         };
         // Отправляем созданную комнату обратно клиенту
         socketIO.emit('roomCreated', newRoom);
-      });
+    });
 
-      socket.on('joinRoom', (roomName) => {
+    socket.on('joinRoom', (roomName) => {
         socket.join(roomName);
     });
 
@@ -51,10 +56,10 @@ socketIO.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log(socket.id + ' disconnect')
-    })
-})
+        console.log(socket.id + ' disconnect');
+    });
+});
 
 http.listen(PORT, () => {
-    console.log('Server working')
-})
+    console.log('Server working');
+});
